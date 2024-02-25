39e34d886dccdc5f7e414e19b015b57481b69f7e
MrDewmon69 <149539322+MrDewmon69@users.noreply.github.com>
2024-02-26 11:57:12 +1300

    update index.py
    Added working dealer

--- index.py
+++ index.py
@@ -12,20 +12,19 @@ class Player:
         self.hand = hand
         
     def startGame(self):
-        global hitButton
+        global hitButton, standButton, dealerLabel, dealerCards
         hitButton = Button(mainWindow, text="Hit", command=player1.hit)
-        hitButton.pack()
+        hitButton.grid(row=1, column=3)
 
-        global standButton
         standButton = Button(mainWindow, text="Stand", command=player1.stand)
-        standButton.pack()
+        standButton.grid(row=1, column=2)
         startGameButton.destroy()
 
         deck = cardDeck
         for e in range(2):
             card = r.choice(deck)
             cards.append(card)
-
+        dealerLabel["text"] = dealerCards
         player1.hand = cards
         cardTotal = cards[0] + cards[1]
         print(cardTotal)
@@ -33,6 +32,7 @@ class Player:
         playerCardLabel["text"] = convertedList
 
     def hit(self):
+        global playerCards
         print(player1.hand)
         playerCards = player1.hand
         newCard = r.choice(cardDeck)
@@ -44,33 +44,56 @@ class Player:
 
         if sum(playerCards) > 21:
             loserLabel = Label(mainWindow, text="You busted")
-            loserLabel.pack()
+            loserLabel.grid(row=5, column=2)
+            hitButton.destroy()
+            standButton.destroy()
 
     def stand(self):
-        global hitButton
+        global dealerLabel, dealerCards
+        dealerBustLabel = Label(mainWindow, text="Game in progress")
+        dealerBustLabel.grid(row=5, column=2) 
         hitButton.destroy()
-        global standButton
         standButton.destroy()
+
+        while sum(dealerCards) <= 17:
+            newDealCard = r.choice(cardDeck)
+            dealerCards.append(newDealCard)
+            dealerLabel["text"] = dealerCards
+
+        if sum(dealerCards) > 21:
+            dealerBustLabel["text"] = "Dealer Busts"
+        elif sum(dealerCards) > sum(cards):
+            dealerBustLabel["text"] = "Dealer Wins"
+        elif sum(dealerCards) < sum(cards):
+            dealerBustLabel["text"] = "Player Wins"
+        elif sum(dealerCards) == sum(cards):
+            dealerBustLabel["text"] = "Draw"
 
 class Dealer:
     def __init__(self, *hand):
         self.hand = hand
 
     def deal(self):
+        global dealerLabel, dealerCards
         dealerCards = []
         dealerFirstCard = r.choice(cardDeck)
         dealerCards.append(dealerFirstCard)
         dealer.hand = dealerCards
         print(dealerCards)
 
+        dealerText = Label(mainWindow, text="Dealer's Cards: ")
+        dealerText.grid(row=4, column=2, columnspan=1)
+        dealerLabel = Label(mainWindow, text="0")
+        dealerLabel.grid(row=4, column=3)
+        
 player1 = Player()
 dealer = Dealer()
 dealer.deal()
 
 startGameButton = Button(mainWindow, text="Deal", command=player1.startGame)
-startGameButton.pack()
+startGameButton.grid(row=1, column=3)
 playerCardLabel = Label(mainWindow, text="0")
-playerCardLabel.pack()
+playerCardLabel.grid(row=2, column=3)
 
 if __name__ == '__main__':
     mainWindow.mainloop()
